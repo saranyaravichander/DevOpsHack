7f0bedecaa2c2e4adbdb71c9299b4a92268376b9
saranyaravichander <saranyaravichander@live.com>
2019-10-07 02:32:37 -0400

    Initial commit

---
+++ NovantHackWebSQL/NovantHackWebSQL/Pages/Index.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace NovantHackWebSQL.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private IConfiguration config;
+
+        public IndexModel(IConfiguration configuration)
+        {
+            config = configuration;
+        }
+
+        public void OnGet()
+        {
+            string connectionString = config.GetSection("Data").GetSection("SQLConnectionString").Value;
+            
+            // Provide the query string with a parameter placeholder.
+            string queryString =
+                "SELECT * from dbo.Test";
+
+            using (SqlConnection connection =
+                new SqlConnection(connectionString))
+            {
+                // Create the Command and Parameter objects.
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                    }
+                    reader.Close();
+
+                    ViewData["SQLStatus"] = "Connected to SQL";
+                }
+                catch (Exception ex)
+                {
+                    ViewData["SQLStatus"] = "Error connecting to SQL";
+                }
+            }
+        }
+    }
+}
